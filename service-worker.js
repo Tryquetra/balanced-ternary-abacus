8e5c472f1313e326ce6791496ea9e9478e0fb8c5
Robson Cassiano <robson.cassiano@pm.me>
2026-09-13 22:33:04 -0300

    fix(cors): remove atributo crossorigin do tailwind e sobe sw para v8

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
   Caches core app shell and provides runtime caching for other requests (e.g., CDN assets).
 */
 
-const CACHE_VERSION = 'v7';
+const CACHE_VERSION = 'v8';
 const APP_CACHE = `abaco-cache-${CACHE_VERSION}`;
 const RUNTIME_CACHE = 'runtime-cache';
 
